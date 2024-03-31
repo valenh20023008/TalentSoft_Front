5ccd33ddf9b4a404de61b8e90d5ad4659a673f9d
Valentina Hernandez Bayona <valenh20023008@gmail.com>
2024-03-30 22:57:41 -0500

    nav2-footer-payment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Landing1Component } from './components/landing1/landing1.component';
 import { LoginRRHHComponent } from './components/login-rrhh/login-rrhh.component';
+import { PaymentComponent } from './components/payment/payment.component';
+
 
 const routes: Routes = [
   { path: '', component: Landing1Component },
   { path: 'login', component: LoginRRHHComponent },
+  {path: 'planes', component: PaymentComponent},
   { path: '**', redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { Landing1Component } from './components/landing1/landing1.component';
 import { FormsModule } from '@angular/forms';
 import { LoginRRHHComponent } from './components/login-rrhh/login-rrhh.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { Navbar2Component } from './components/navbar2/navbar2.component';
+import { Footer2Component } from './components/footer2/footer2.component';
+import { PaymentComponent } from './components/payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     Landing1Component,
-    LoginRRHHComponent
+    LoginRRHHComponent,
+    FooterComponent,
+    Navbar2Component,
+    Footer2Component,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
